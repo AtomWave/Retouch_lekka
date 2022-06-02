e5d2281def89a68bcee49bf35d6e99ae847add07
AtomWave <AtomWave@yandex.ru>
2022-06-02 16:56:45 +0300

    Адаптив сайта

--- js/main.js
+++ js/main.js
@@ -38,5 +38,18 @@ $(function() {
       autoplaySpeed: 2000,
    });
 
+   $(".menu, .bike__column, .bike__link").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
 
 });
