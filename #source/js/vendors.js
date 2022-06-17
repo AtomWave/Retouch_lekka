49691d8817db6a2fa5dd3f83d918b2026cda26d5
AtomWave <78145765+AtomWave@users.noreply.github.com>
2022-06-17 16:17:42 +0300

    ааа

--- #source/js/vendors.js
+++ #source/js/vendors.js
@@ -1,46 +1,18 @@
 
-   $('.bike-slider').slick({
-      arrows: false,
-      dots: true,
-      fade: true,
-      autoplay: true,
-      autoplaySpeed: 2000,
-   });
-
-   $('.slider__items').slick({
-      arrows: false,
-      dots: true,
-      fade: false,
-      autoplay: true,
-      autoplaySpeed: 2000,
-   });
-
-   $(".menu, .bike__column, .bike__link").on("click","a", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
-		event.preventDefault();
-
-		//забираем идентификатор бока с атрибута href
-		var id  = $(this).attr('href'),
-
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
-			top = $(id).offset().top;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1000);
-	});
+  
 
    // Плавающая кнопка 
-   $(window).scroll(function(){
-      if ($(this).scrollTop() > 100) {
-      $('.scrollup').fadeIn();
-      } else {
-      $('.scrollup').fadeOut();
-      }
-      });
+  //  $(window).scroll(function(){
+  //     if ($(this).scrollTop() > 100) {
+  //     $('.scrollup').fadeIn();
+  //     } else {
+  //     $('.scrollup').fadeOut();
+  //     }
+  //     });
        
-      $('.scrollup').click(function(){
-      $("html, body").animate({ scrollTop: 0 }, 600);
-      return false;
-      });
+  //     $('.scrollup').click(function(){
+  //     $("html, body").animate({ scrollTop: 0 }, 600);
+  //     return false;
+  //     });
 
    // 
